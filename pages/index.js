5c99d6f0166b26365395158eb8d8d6692e4b018c
codethebasics <codethebasics@gmail.com>
2022-07-18 21:01:43 -0300

    Login page template scss

--- pages/index.js
+++ pages/index.js
@@ -3,6 +3,7 @@ import styles from "/styles/LoginPage.module.scss";
 export default function Home() {
   return (
     <div className={styles.loginForm}>
+      <h1>Welcome</h1>
       <label>Login</label>
       <input name="username" />
       <label>Password</label>
